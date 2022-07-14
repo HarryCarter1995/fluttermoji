c7c14e1a631d28ecdd04a2164dcd03e918867c56
HarryCarter1995 <70072521+HarryCarter1995@users.noreply.github.com>
2022-07-15 00:52:07 +0100

    Update fluttermojiCustomizer.dart

--- lib/fluttermojiCustomizer.dart
+++ lib/fluttermojiCustomizer.dart
@@ -113,7 +113,7 @@ class _FluttermojiCustomizerState extends State<FluttermojiCustomizer>
     var _fluttermojiController;
     Get.put(FluttermojiController());
     _fluttermojiController = Get.find<FluttermojiController>();
-    _fluttermojiController.selections.value = widget.selections;
+    _fluttermojiController.updateSelections(widget.selections);
     setState(() {
       tabController = TabController(length: attributesCount, vsync: this);
       fluttermojiController = _fluttermojiController;
