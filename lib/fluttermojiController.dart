083929cde9991be780eacbdbb70132a77be92632
HarryCarter1995 <70072521+HarryCarter1995@users.noreply.github.com>
2022-07-15 00:33:49 +0100

    Update fluttermojiController.dart

--- lib/fluttermojiController.dart
+++ lib/fluttermojiController.dart
@@ -21,7 +21,7 @@ import 'fluttermoji_assets/top/hairStyles/hairStyle.dart';
 /// Exposes certain static functions for use by the developer
 class FluttermojiController extends GetxController {
   var fluttermoji = "".obs;
- String selections;
+ String selections = "";
 
   /// Stores the option selected by the user for each attribute
   /// where the key represents the Attribute
