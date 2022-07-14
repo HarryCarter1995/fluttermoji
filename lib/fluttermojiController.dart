e82012fb86d2bf5d7a3ac2be20a520bb44f7b337
HarryCarter1995 <70072521+HarryCarter1995@users.noreply.github.com>
2022-07-14 23:25:40 +0100

    Update fluttermojiController.dart

--- lib/fluttermojiController.dart
+++ lib/fluttermojiController.dart
@@ -92,6 +92,7 @@ class FluttermojiController extends GetxController {
     fluttermoji.value = fluttermojiNew;
     await pref.setString(
         'fluttermojiSelectedOptions', jsonEncode(selectedOptions));
+	print('Selected options: ${jsonEncode(selectedOptions)}');
     update();
   }
 
